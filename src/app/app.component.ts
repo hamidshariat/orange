fc0cb388bab611e0ee22411f9af92654e946f1bc
khalajabdollah <hesamkhalaj2@gmail.com>
2024-12-02 08:21:21 -0800

    bug fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DrawComponent } from '../draw/draw.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DrawComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
