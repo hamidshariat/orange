1f3cfe2417dbca77083a0ac7bfa804cddf749eaf
H.ShariatMovahed <hamidshariatwebdeveloper@gmail.com>
2024-12-02 19:38:14 +0330

    create component

---
+++ src/draw/draw.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-draw',
+  imports: [],
+  templateUrl: './draw.component.html',
+  styleUrl: './draw.component.css'
+})
+export class DrawComponent {
+
+}
